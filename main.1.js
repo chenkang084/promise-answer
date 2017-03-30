62bedeb1bc7439493b9bd921dd0c2abb7648a608
chenkang1 <chenkang1@lenovo.com>
2017-03-30 22:27:15 +0800

    optimize

--- main.1.js
+++ main.1.js
@@ -1,32 +1,22 @@
 import 'whatwg-fetch'
 
 function getData(url) {
-    return fetch(url)
-        .then(response => response.ok ? response.json() : { "err": "Network response was not ok." })
-        .catch(error => ({ "err": "There has been a problem with your fetch operation: " + error.message }))
+    return fetch(url).then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return {
+                "err": "Network response was not ok."
+            }
+        }
+    }).catch((error) => {
+        return {
+            "err": "There has been a problem with your fetch operation: " + error.message
+        }
+    }).then((result) => {
+        return result;
+    })
 }
-
-function getDataAsync(urlSplit) {
-    return Promise.all(
-        urlSplit.map(url => getData(url))
-    )
-}
-
-async function loadDataTask(urls, split = 2) {
-    let urlList = [...urls];
-    let results = [];
-
-    while (urlList.length > 0) {
-        let urlSplit = urlList.splice(0, split);
-        console.log('start')
-        let data = await getDataAsync(urlSplit);
-        console.log('end')
-        results = results.concat(data);
-    }
-
-    return results;
-};
-
 
 let urls = [
     "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo",
@@ -41,6 +31,29 @@ let urls = [
     "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo",
 ]
 
-loadDataTask(urls, 2).then((arr) => {
-    console.log(arr)
-});
+async function setPromiseArr(urls) {
+    let promiseArr = urls.map((url) => {
+        return getData(url)
+    })
+    return await Promise.all([...promiseArr])
+}
+
+function sendRequests(urls, split = 2, result = []) {
+    let urlsList = [];
+
+    if (urls.length > 0) {
+        urlsList = urls.splice(0, split)
+        console.log('send %s urls start', split);
+        setPromiseArr(urlsList).then((data) => {
+            result = result.concat(data)
+            console.log('get %s urls result= %s', split, result);
+            sendRequests(urls, split, result);
+        })
+    } else {
+        console.log(result);
+        return result;
+    }
+}
+
+let result = sendRequests(urls);
+console.log(result);
--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-exports.name = 'jack'
-exports.age = 10
+// exports.name = 'jack'
+// exports.age = 10
+
+// export const sex = 'boy'
+// const fs = require('fs')
+
+// var Thunk = function(fn){
+//   return function (){
+//     var args = Array.prototype.slice.call(arguments);
+//     return function (callback){
+//       args.push(callback);
+//       return fn.apply(this, args);
+//     }
+//   };
+// };
+
+// var readFileThunk = Thunk(fs.readFile);
+// readFileThunk(fileA)(callback);
 
